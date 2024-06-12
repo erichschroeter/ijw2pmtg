781c46484e9b106780556d336cdc079a5f6c7539
Erich Schroeter <erich.schroeter@gmail.com>
2024-06-12 15:46:30 -0500

    feat: download cards from Scryfall.com

---
+++ README.md
@@ -0,0 +1,12 @@
+# Scryfall Scripts
+
+Scripts for downloading stuff from [Scryfall](https://scryfall.com).
+
+## Setup
+
+```
+python3 -m venv .env
+source .env/bin/activate
+pip install -r requirements.txt
+python3 -m scryfall --help
+```
---
+++ scryfall/cli.py
@@ -0,0 +1,88 @@
+import argparse
+import logging
+
+#region Command line parsing  # noqa
+
+
+class ColorLogFormatter(logging.Formatter):
+    '''
+    Custom formatter that changes the color of logs based on the log level.
+    '''
+
+    grey = "\x1b[38;20m"
+    green = "\u001b[32m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    blue = "\u001b[34m"
+    cyan = "\u001b[36m"
+    reset = "\x1b[0m"
+
+    timestamp = '%(asctime)s - '
+    loglevel = '%(levelname)s'
+    message = ' - %(message)s'
+
+    FORMATS = {
+        logging.DEBUG:    timestamp + blue + loglevel + reset + message,
+        logging.INFO:     timestamp + green + loglevel + reset + message,
+        logging.WARNING:  timestamp + yellow + loglevel + reset + message,
+        logging.ERROR:    timestamp + red + loglevel + reset + message,
+        logging.CRITICAL: timestamp + bold_red + loglevel + reset + message
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+def _init_logger(level=logging.INFO):
+    logger = logging.getLogger()
+    logger.setLevel(level)
+
+    formatter = ColorLogFormatter()
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+
+class RawTextArgumentDefaultsHelpFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawTextHelpFormatter):
+    pass
+
+
+#endregion Command line parsing  # noqa
+
+class App:
+    def __init__(self) -> None:
+        self.args = None
+        self.parser = argparse.ArgumentParser(prog='scryfall')
+        self.parser.add_argument('-v', '--verbosity',
+                                 choices=['critical', 'error', 'warning', 'info', 'debug'],
+                                 default='info',
+                                 help='Set the logging verbosity level.')
+        self.parser.add_argument('--server', default='scryfall.com', help="The Scryfall server URL.")
+        self.parser.set_defaults(func=download_cards)
+
+    def parse_args(self, args=None):
+        self.args = self.parser.parse_args(args)
+
+    def run(self):
+        if not self.args:
+            self.parse_args()
+        _init_logger(getattr(logging, self.args.verbosity.upper()))
+        logging.debug(f'command-line args: {self.args}')
+        self.args.func(self.args)
+
+
+def download_cards(args):
+    logging.info('downloading cards')
+
+
+def main():
+    App().run()
+
+
+if __name__ == '__main__':
+    main()
